7b20287968431652817d369f540e12837552fd9c
Lee-jong-won <78682445+Lee-jong-won@users.noreply.github.com>
2026-04-08 23:10:54 +0900

    chore : 커넥션 풀 크기 조정

--- src/test/java/jongwon/e_commerce/medium/PaymentApprovalControllerTest.java
+++ src/test/java/jongwon/e_commerce/medium/PaymentApprovalControllerTest.java
@@ -44,7 +44,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
 @ActiveProfiles("test")
 public class PaymentApprovalControllerTest {
 
